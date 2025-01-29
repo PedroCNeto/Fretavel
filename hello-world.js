9c60a9a0e1f54523fc196ee0f553ea03fc3f4687
PedroCNeto <pedrocastelarineto@gmail.com>
2025-01-29 11:37:52 -0300

    Primeiro Envio

---
+++ hello-world.js
@@ -0,0 +1,16 @@
+const http = require('node:http');
+const dt = require('./date.js');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
